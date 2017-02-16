40c3a320f207b78faaccbd32cd3be6001313089a
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2017-02-16 17:54:33 +0800

    Created temporary book

--- index.php
+++ index.php
@@ -23,9 +23,18 @@ if (matches('/^\/books$/', $uri)) {
     // POST /books
     if (is_method('POST')) {
       // TODO: ... add a book
-      
+
+      // Create temporary book
+      $book = [
+        'name'   => $params['name'],
+        'author' => $params['author']
+      ];
+
       // Set response code 201 CREATED
       http_response_code(201);
+
+      // Return book in JSON form
+      return json_encode($book);
     }
 }
 
