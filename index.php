db1511ed31cf6f7bc35c85b70e4d16cd5e4d7150
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2017-02-19 11:20:33 +0800

    Created table

--- index.php
+++ index.php
@@ -4,18 +4,28 @@
 $uri = $_SERVER['REQUEST_URI'];
 
 // DB connection arguments
-$servername = "localhost";
+$serverName = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "books";
+$dbName     = "books";
 
 // Start DB connection
-$dbCon = mysqli_connect($servername, $username, $password);
+$dbCon = mysqli_connect($serverName, $username, $password);
 
 // Create Database
-$dbQuery = "CREATE DATABASE IF NOT EXISTS books";
-
+$dbQuery = "CREATE DATABASE IF NOT EXISTS " . $dbName;
 mysqli_query($dbCon, $dbQuery);
+
+// Select Database
+mysqli_select_db($dbCon, $dbName);
+
+// Create Table
+$tableQuery = "CREATE TABLE IF NOT EXISTS books (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  author VARCHAR(50) NOT NULL
+)";
+mysqli_query($dbCon, $tableQuery);
 
 function is_method($method) {
   return $_SERVER['REQUEST_METHOD'] === $method;
